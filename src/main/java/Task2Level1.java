d8067d463cfed1d3a00fd5b2aab1e95bb53042b3
Nikolaj Redko <nikolajred@gmail.com>
2019-11-08 21:47:46 +0200

    Third commit four task solved

--- src/main/java/Task2Level1.java
+++ src/main/java/Task2Level1.java
@@ -1,3 +1,61 @@
 public class Task2Level1 {
+    private static int deskSize = 8;
+    private static int[][] desk = new int[deskSize][deskSize];
+    private static int i = 0;
+    private static int j = 0;
+    private static int count = 1;
+    private static int[] iStep = {2, 2, -2, -2, 1, -1, 1, -1};
+    private static int[] jStep = {1, -1, -1, 1, -2, -2, 2, 2};
+    private static int k;
+    private static int[] minStep = {8, 0, 0};
 
+    public static void main (String[] args){
+        for (i = 0; i < deskSize; i++){
+            for ( j = 0; j < deskSize; j++) {
+                desk[i][j] = 0;
+            }
+        }
+        i = 0;
+        j = 0;
+        desk[i][j] = 1;
+        for (count=2; count<=Math.pow(deskSize, 2); count++){
+            minStep[0] = 8;
+            for (k=0; k<8; k++){
+                if (0<=(i+iStep[k]) && (i+iStep[k])<deskSize &&
+                        0<=(j+jStep[k]) && (j+jStep[k])<deskSize &&
+                        desk[i+iStep[k]][j+jStep[k]]==0 &&
+                        CheckHS(i+iStep[k],j+jStep[k])<=minStep[0]){
+                    minStep[0] = CheckHS(i+iStep[k],j+jStep[k]);
+                    minStep[1] = i+iStep[k];
+                    minStep[2] = j+jStep[k];
+                }
+            }
+            i = minStep[1];
+            j = minStep[2];
+            desk[i][j] = count;
+        }
+        for (i = 0; i < deskSize; i++){
+            for (j = 0; j < deskSize; j++) {
+                if (desk[i][j] == 0){
+                    System.out.print("   X");
+                } else {
+                    System.out.format("%4d",desk[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static int CheckHS (int i, int j){
+        int steps = 0;
+        int n;
+
+        for (n=0; n<8; n++){
+            if (0<=(i+iStep[n]) && (i+iStep[n])<deskSize && 0<=(j+jStep[n]) &&
+                    (j+jStep[n])<deskSize && desk[i+iStep[n]][j+jStep[n]]==0){
+                steps++;
+            }
+        }
+        return steps;
+    }
 }
---
+++ src/test/java/Task2Level2Test.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task2Level2Test {
+
+    @Test
+    public void maxDepthTree() {
+        Task2Level2 task2Level2 = new Task2Level2();
+        int actual = task2Level2.maxDepthTree();
+
+        int expected = ;
+    }
+    @Test
+    public void maxDepthTreeNull() {
+        Task2Level2 task2Level2 = new Task2Level2();
+        int actual = task2Level2.maxDepthTree();
+        int expected = ;
+    }
+}
